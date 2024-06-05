2ad816f8403287d7c2d7b92197e428d7ca666cca
Benjamin Goudey <bgoudey@unimelb.edu.au>
2024-06-05 12:05:18 +1000

    Updating notebook. Data is now has no missing

--- preprocess.R
+++ preprocess.R
@@ -4,16 +4,19 @@ library(tidyverse)
 x=readxl::read_xlsx('./dataset/RC Health Care Data-20180820.xlsx')
 filtered_dat=x %>% 
   janitor::clean_names() %>% 
-  select(-drinking_status_1_current_drinker_2_ever_drinker_3_never_drinker, 
+  dplyr::select(-drinking_status_1_current_drinker_2_ever_drinker_3_never_drinker, 
          -smoking_status_1_current_smoker_2_ever_smoker_3_never_smoker, 
          -ast_u_l,
          -family_histroy_of_diabetes_1_yes_0_no,
          -diabetes_diagnosed_during_followup_1_yes)  %>% 
   filter(!is.na(hdl_c_mmol_l))  %>% 
-  mutate(diabetes=censor_of_diabetes_at_followup_1_yes_0_no)
+  mutate(diabetes=censor_of_diabetes_at_followup_1_yes_0_no) %>% 
+  dplyr::select(-fpg_of_final_visit_mmol_l,
+                -censor_of_diabetes_at_followup_1_yes_0_no) %>% 
+      filter(complete.cases(.))
 
 # Find matched group
-matchit.out=MatchIt::matchit(censor_of_diabetes_at_followup_1_yes_0_no ~ age_y +
+matchit.out=MatchIt::matchit(diabetes ~ age_y +
                      gender_1_male_2_female + 
                      site, data=filtered_dat,
                    method = "nearest", distance ="glm",ratio = 1,replace = FALSE)
@@ -25,6 +28,8 @@ matched_filtered_dat = filtered_dat[c(as.numeric(I$match.matrix),
   ungroup() 
 
 dat<-matched_filtered_dat %>% 
-  select(-site) 
+  dplyr::select(-site)
 
-write.csv(dat, file = './RC_health_data_n2000.csv')
+#dat
+
+write.csv(dat, file = './RC_health_data_n2000.csv', row.names = F)
